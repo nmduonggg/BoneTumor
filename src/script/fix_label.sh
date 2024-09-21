37532ad6b1b2328ccd0cd14b46dd1f509a161ac6
nmduonggg <nmduong2104@gmail.com>
2024-09-21 10:48:50 +0700

    fix label

--- src/script/fix_label.sh
+++ src/script/fix_label.sh
@@ -3,4 +3,4 @@ python fix_label.py \
     --weight_path '~/aiotlab/nmduong/BoneTumorsrc/exp_cls_256_cls_rm_confused_case_with9/UNI_lora_cls/_best.pt' \
     --labels_dir '~/aiotlab/nmduong/BoneTumor/RAW_DATA/labels/' \
     --images_dir '~/aiotlab/nmduong/BoneTumor/RAW_DATA/images/' \
-    --old_labels_dir './infer/smooth/Visualizations' \
+    --old_labels_dir './infer/Visualizations' \
