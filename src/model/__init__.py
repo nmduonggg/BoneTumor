1b1cf64453240e898ea69368aa8fd758571ae9c3
nmduonggg <nmduong2104@gmail.com>
2024-10-03 21:02:02 +0700

    update vit baseline

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,12 +1,15 @@
 from __future__ import absolute_import
 # from .mobilenetv2 import mobilenetv2
 from .UNI_lora_cls import UNI_lora_cls
+from .ViT_baseline import ViT_baseline
 from .Transformer_reorder import TransformerReorder
 from .Stacked_model import StackedModel
 
 def create_model(opt):
     if opt['network_G']['which_model_G'] == 'uni_lora_cls':
         return UNI_lora_cls(opt['network_G']['out_nc'])
+    elif opt['network_G']['which_model_G'] == 'vit_baseline':
+        return ViT_baseline(opt['network_G']['out_nc'])
     elif opt['network_G']['which_model_G'] == 'transformer_reorder':
         return TransformerReorder()
     elif opt['network_G']['which_model_G'] == 'stacked_model':
