d6dc0711e2bce5e0f98052229e99d57af352a54f
nmduonggg <nmduong2104@gmail.com>
2024-10-11 11:59:00 +0700

    update: crop images for phas2 using labels

---
+++ RAW/process/phase2/crop_regions_following_labels.sh
@@ -0,0 +1,6 @@
+python crop_regions_following_labels.py \
+    --n 10 \
+    --image_folder '/mnt/disk4/nmduong/Vin-Uni-Bone-/images' \
+    --labels_folder '/mnt/disk4/nmduong/Vin-Uni-Bone-/labels' \
+    --output_folder '/mnt/disk4/nmduong/Vin-Uni-Bone-Tumor/BoneTumor/RAW/REAL_WSIs/phase2_training_data' \
+    # --start_metadata '/mnt/disk1/nmduong/Vin-Uni-Bone-Tumor/BoneTumor/RAW/REAL_WSIs/phase2_training_data/Case_1/metadata.json'
