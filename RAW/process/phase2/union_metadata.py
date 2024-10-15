d23ac120abd7398dcb1a9de257a009619f9fe3af
nmduonggg <nmduong2104@gmail.com>
2024-10-15 16:11:42 +0700

    union metatdata for phase 2

---
+++ RAW/process/phase2/union_metadata.py
@@ -0,0 +1,34 @@
+import os
+import json
+
+def read_json(json_path):
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def write_json(data, json_path):
+    with open(json_path, 'w') as f:
+        json.dump(data, f, indent=4)
+    return 
+
+if __name__=='__main__':
+    root_folder = '/mnt/disk4/nmduong/Vin-Uni-Bone-Tumor/BoneTumor/RAW/REAL_WSIs/phase2_training_data'
+    out_path = '/mnt/disk4/nmduong/Vin-Uni-Bone-Tumor/BoneTumor/RAW/REAL_WSIs/phase2_training_data/metadata.json'
+    
+    all_metadatas = []
+    cnt = 0
+    for case in os.listdir(root_folder):
+        if "Case" not in case: continue
+        print("Process:", case)
+        case_folder = os.path.join(root_folder, case)
+        metadata_path = os.path.join(case_folder, 'metadata.json')
+        
+        metadata = read_json(metadata_path)
+        for item_infor in metadata:
+            item_infor['case'] = case
+            all_metadatas.append(item_infor)
+            cnt += 1
+    
+    write_json(all_metadatas, out_path)
+    
+    print(f"TOTAL - {cnt} images")
