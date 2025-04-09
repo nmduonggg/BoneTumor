e40ab80e7d6db643e1c35ec649cc93d17bc613a3
nmduonggg <nmduong2104@gmail.com>
2025-04-10 01:49:39 +0700

    add swinv2 training script

---
+++ src/script/_WRAPPED_SCRIPTS/train/wrapped_train_swinv2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=train_swinv2       # Job name
+#SBATCH --output=log_slurm/result/train_swinv2.txt      # Output file
+#SBATCH --error=log_slurm/error/train_swinv2.txt        # Error file
+#SBATCH --ntasks=1               # Number of tasks (processes)
+#SBATCH --gpus=1                 # Number of GPUs per node
+#SBATCH --nodes=1               # Số node yêu cầu
+#SBATCH --cpus-per-task=8       # Số CPU cho mỗi task
+conda init
+conda activate nmduong2
+cd /home/user01/aiotlab/nmduong/BoneTumor/src
+bash ./script/PILOT/train/baseline/train_swinv2.sh
