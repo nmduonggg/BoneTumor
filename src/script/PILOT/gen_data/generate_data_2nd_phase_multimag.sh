fba1e57307e6e86dcd939da3207aac02e1a32ffb
Duong Nguyen <nmduongg@your.hostname.com>
2025-07-19 07:17:02 +0000

    add script multimag data phase2 gen

--- src/script/PILOT/gen_data/generate_data_2nd_phase_multimag.sh
+++ src/script/PILOT/gen_data/generate_data_2nd_phase_multimag.sh
@@ -1,4 +1,4 @@
-python generate_data_2nd_phase.py \
+python generate_data_2nd_phase_multimag.py \
     -opt options/UNI_lora/uni_lora_cls_heavy_multimag_ISBI.yml \
     --weight_path '/home/user01/aiotlab/nmduong/BoneTumor/src/exp_cls_256_cls_retrain_with9_rm_confused_cases_multimag/UNI_lora_cls_multimag_ISBI/_best.pt' \
     --data_dir '/home/user01/aiotlab/nmduong/BoneTumor/RAW/REAL_WSIs/phase2_training_data' \
