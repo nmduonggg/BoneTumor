b4d8a18380b00e3f25e09025026f18ad06c1bd7a
Duong Nguyen <nmduongg@your.hostname.com>
2025-09-16 20:06:33 +0000

    fix hier forward

--- src/model/UNI_lora_multimag_ISBI.py
+++ src/model/UNI_lora_multimag_ISBI.py
@@ -121,7 +121,7 @@ class UNI_lora_multimag_ISBI(nn.Module):
         self.transform = transforms.Compose(
             [
                 # transforms.Resize(224),
-                transforms.ToTensor(),
+                # transforms.ToTensor(),
                 transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             ]
         )
