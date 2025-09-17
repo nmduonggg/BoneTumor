7171d15ab69d4d782079ca8d6dfb229ad772596e
Duong Nguyen <nmduongg@your.hostname.com>
2025-09-17 02:48:48 +0000

    fix shape

--- src/model/UNI_lora_multimag_ISBI.py
+++ src/model/UNI_lora_multimag_ISBI.py
@@ -152,7 +152,10 @@ class UNI_lora_multimag_ISBI(nn.Module):
         y_im1s = self.forward(x_im0, x_im1, None) # BxCl
         print("CKPT 4")
             # print(y_im2s)
-        y_im1s = torch.ones([x_im1.size(0), 1, x_im1.size(2), x_im1.size(3)]).to(device) * y_im1s.reshape(len(im1s), -1, 1, 1)  # BxClxHxW
+        
+        print(y_im1s.shape)
+            
+        y_im1s = torch.ones([x_im1.size(0), 1, x_im1.size(2), x_im1.size(3)]).to(device) * y_im1s.reshape(x_im1.size(0), -1, 1, 1)  # BxClxHxW
         print(y_im1s.shape)
         y_im0 = utils.combine_output(y_im1s, num_h1, num_w1, h1, w1, 128, 128, channel=self.out_nc)
         
