fc4f711cf230faa4e02c1c88d1352a46d4ae062b
Duong Nguyen <nmduongg@your.hostname.com>
2025-09-24 21:32:28 +0000

    update bbdm train script

---
+++ src/script/_WRAPPED_SCRIPTS/train/wrapped_train_bbdm_phase2_multi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=train_bbdm_multi        # Job name
+#SBATCH --output=log_slurm/result/train_bbdm_multi.txt      # Output file
+#SBATCH --error=log_slurm/error/train_bbdm_multi.txt        # Error file
+#SBATCH --ntasks=1               # Number of tasks (processes)
+#SBATCH --gpus=1                 # Number of GPUs per node
+#SBATCH --nodes=1               # Số node yêu cầu
+#SBATCH --cpus-per-task=8       # Số CPU cho mỗi task
+conda init
+conda activate nmduong2
+cd /home/nmduongg/BoneTumor/works/BoneTumor/BBDM2
+bash ./script/discreteBBDM/train_discrete_bbdm_multimag.sh
